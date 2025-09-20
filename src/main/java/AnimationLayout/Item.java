a133230fc3caaca9dcb85438248ee90914184dad
Juan Monta <jcdbz66@outlook.com>
2025-09-20 11:04:47 -0500

    Actualizando la documentacion de la clase Item

--- src/main/java/AnimationLayout/Item.java
+++ src/main/java/AnimationLayout/Item.java
@@ -25,36 +25,37 @@ public abstract class Item extends JPanel implements DnD_AnimationTimeCurrentVal
     private ToggleList toggleList;
 
     /**
-     * Listener colocado automáticamente por el ToggleAnimationLayout. No
-     * actualizar.
+     * Listener colocado automáticamente por el
+     * {@link ToggleListAnimationLayout}. No actualizar.
      *
-     * @param listener
+     * @param listener {@link UnD_MovimientoListener}
      */
-    public void setMovimientoListener(UnD_MovimientoListener listener) {
+    void setMovimientoListener(UnD_MovimientoListener listener) {
         this.movimientoListener = listener;
     }
 
     /**
-     * Empezar el movimiento del item en la direcion propuesta.
+     * Empezar el movimiento del {@link Item} en la direcion propuesta.
      *
-     * @param direccion UP=-1 o DOWN=1
+     * @param direccion UP=-1 o DOWN=1 usando valores de la clase {@link UnD_MovimientoDireccion#UP}
+     * {@link UnD_MovimientoDireccion#DOWN}
      */
     public void moverItem(int direccion) {
         this.movimientoListener.moverItem(this, direccion);
     }
 
     /**
-     * Listener colocado autompaticamente por el ToggleAnimationLayout. No
-     * actualizar.
+     * Listener colocado autompaticamente por {@link ToggleListAnimationLayout}.
+     * No actualizar.
      *
-     * @param listener
+     * @param listener {@link UnD_EliminacionListener}
      */
-    public void setEliminacionListener(UnD_EliminacionListener listener) {
+    void setEliminacionListener(UnD_EliminacionListener listener) {
         this.eliminacionListener = listener;
     }
 
     /**
-     * Inicia la eliminación del item mediante una animación.
+     * Inicia la eliminación de este {@link Item} mediante una animación.
      */
     public void deleteItem() {
         //Listener que se agrega automáticamente por el ToggleAnimationLayout.
@@ -67,9 +68,6 @@ public abstract class Item extends JPanel implements DnD_AnimationTimeCurrentVal
      * Lista de subItems que contiene el Item en cuestion.
      */
     private final List<Item.SubItem> subItemsList;
-    /**
-     * Para colocarle un ID al Item. Default = ""
-     */
     private String itemId = "", itemTitle = "", itemSubTitle = "", itemInfo01 = "", itemInfo02 = "", itemInfo03 = "";
     private int itemIndex = -1;
 
@@ -79,10 +77,21 @@ public abstract class Item extends JPanel implements DnD_AnimationTimeCurrentVal
         this.itemDragAndDropActivar = new DnD_Activar(Item.this, this);
     }
 
+    /**
+     * Index del {@link Item} que se la ha asignado en el layout.
+     *
+     * @return
+     */
     public int getItemIndex() {
         return itemIndex;
     }
 
+    /**
+     * Index que se le asignado automáticamente en el
+     * {@link ToggleListAnimationLayout}
+     *
+     * @param itemIndex
+     */
     void setItemIndex(int itemIndex) {
         this.itemIndex = itemIndex;
     }
@@ -146,47 +155,59 @@ public abstract class Item extends JPanel implements DnD_AnimationTimeCurrentVal
     }
 
     /**
-     * Clase donde se manejas los Items y SubItems
+     * Clase donde se manejan los {@link Item} y {@link Item.SubItem}
      */
     private static ToggleListAnimationLayout toggleListLayout;
 
     /**
-     * Obtener la clase donde se manejan los Items y SubItems, Se asigna de
-     * manera automática, no tocar.
+     * Obtener la clase donde se manejan los {@link Item} y
+     * {@link Item.SubItem}. Se asigna de manera automática, no tocar.
      *
      * Principalmente aquí para las animaciones de mostrar u ocultar los
      * subItems del Item.
      *
-     * @return
+     * @return instancia de {@link ToggleListAnimationLayout}
      */
     ToggleListAnimationLayout getToggleListLayout() {
         return Item.toggleListLayout;
     }
 
     /**
-     * Obtener la clase que hace la función de mostrar u ocultar los subItems
-     * del Item.
+     * Obtener la clase que hace la función de mostrar u ocultar los
+     * {@link Item.SubItem} del {@link Item} .
      *
-     * @return
+     * @return instancia de {@link ToggleList}
      */
     ToggleList getToggleList() {
         return this.toggleList;
     }
 
+    /**
+     * Controla la visibilidad de los {@link Item.SubItem} como OCULTO o VISIBLE
+     * {@link ToggleList#setShowing(boolean)}, el valor cambiará al
+     * correspondiente inverso si está OCULTO o VISIBLE en ese momento.
+     */
     public void toggleList() {
         this.getToggleList().setShowing(!this.getToggleList().isShowing(), true);
     }
 
+    /**
+     * Controla la visibilidad de los {@link Item.SubItem} como OCULTO o VISIBLE
+     * {@link ToggleList#setShowing(boolean)}.
+     *
+     * @param setShowing TRUE = VISIBLE / FALSE = OCULTO.
+     * @param animated TRUE = Animar setShowing / FALSE = No animar setShowing
+     */
     public void toggleList(boolean setShowing, boolean animated) {
         this.getToggleList().setShowing(setShowing, animated);
     }
 
     /**
-     * Agregar un subItem al Item.
+     * Agrega un {@link Item.SubItem} al {@link Item}.
      *
-     * @param subItem Item.SubItem a agregar a la lista.
+     * @param subItem {@link Item.SubItem} a agregar al {@link Item}.
      */
-    public void addSubItem(Item.SubItem subItem) {
+    public synchronized void addSubItem(Item.SubItem subItem) {
         subItem.setSubItemIndex(this.subItemsList.size());
         this.subItemsList.add(subItem);
         this.subItemAdded(subItem);
@@ -194,81 +215,139 @@ public abstract class Item extends JPanel implements DnD_AnimationTimeCurrentVal
     }
 
     /**
-     * Agregar subItems al Item.
+     * Agregar varios {@link Item.SubItem} al {@link Item}.
      *
-     * @param subItems
+     * @param subItems Lista de {@link Item.SubItem} a agregar al {@link Item}.
      */
-    public void addSubItems(List<Item.SubItem> subItems) {
+    public synchronized void addSubItems(List<Item.SubItem> subItems) {
         subItems.forEach(subItem -> {
             this.addSubItem(subItem);
         });
     }
 
-    public void itemAdded() {
+    /**
+     * Notifica cuando este {@link Item} ha sido asignado al layout.
+     */
+    public synchronized void itemAdded() {
+    }
+
+    /**
+     * Se lanza cada que un {@link Item.SubItem} es agregado al {@link Item}.
+     *
+     * @param parent El {@link Item} padre del {@link Item.SubItem} añadido.
+     * @param subItemAdded El {@link Item.SubItem} que ha sido añadido al
+     * {@link Item}
+     */
+    public synchronized void subItemAdded(Item parent, Item.SubItem subItemAdded) {
+    }
+
+    /**
+     * Se lanza cada que un {@link Item.SubItem} es agregado al {@link Item}.
+     *
+     * @param subItemAdded El {@link Item.SubItem} que ha sido añadido al
+     * {@link Item}.
+     */
+    public synchronized void subItemAdded(Item.SubItem subItemAdded) {
+    }
+
+    /**
+     * Se lanza cada que un {@link Item.SubItem} es eliminado del {@link Item}.
+     *
+     * @param parent El {@link Item} padre del {@link Item.SubItem} eliminado.
+     * @param subItemDeleted El {@link Item.SubItem} que ha sido eliminado del
+     * {@link Item}.
+     */
+    public synchronized void subItemDeleted(Item parent, Item.SubItem subItemDeleted) {
 
     }
 
     /**
-     * Se lanza cada que un SubItem es agregado al Item.
+     * Se lanza cada que un {@link Item.SubItem} es eliminado del {@link Item}.
      *
-     * @param subItemAdded
+     * @param subItemDeleted El {@link Item.SubItem} que ha sido eliminado del
+     * {@link Item}.
      */
-    public void subItemAdded(Item.SubItem subItemAdded) {
+    public synchronized void subItemDeleted(Item.SubItem subItemDeleted) {
+
     }
-    public void subItemDeleted(Item.SubItem subItemDeleted){
-        
-    }
+
     /**
-     * Obtener la lista de los subItems del Item.
+     * Obtener la lista de los {@link Item.SubItem} del {@link Item}.
      *
-     * @return Lista de subItems del Item.
+     * @return Lista de {@link Item.SubItem} del {@link Item}.
      */
-    public List<Component> getSubItems() {
+    public synchronized List<Component> getSubItems() {
         return (List<Component>) (List<?>) this.subItemsList;
     }
 
     /**
-     * Obtener la cantidad de subItems del Item.
+     * Obtener la cantidad de {@link Item.SubItem} del {@link Item}.
      *
-     * @return numero de subitems del Item.
+     * @return numero de {@link Item.SubItem} del {@link Item}.
      */
-    public int getSubItemsCount() {
+    public synchronized int getSubItemsCount() {
         return this.subItemsList.size();
     }
 
     //##########################################################################
     //PARA LA ANIMACION DRAG AND DROP-------------------------------------------
+    /**
+     * Lanzado mientras dura la animación de activación del drag and drop del
+     * {@link Item}, asignado con {@link #setDragAndDropAnimationTime(int)}
+     *
+     * @param animatedVal Valor acorde a la duración de
+     * {@link #setDragAndDropAnimationTime(int)}
+     */
     @Override
     public void dragAndDropAnimationTimeCurrentValue(float animatedVal) {
 
     }
 
+    /**
+     * Inicia la duración de activación del drag and drop del {@link Item},
+     * asignado con {@link #setDragAndDropAnimationTime(int)}.
+     */
     public void dragAndDropInit() {
         this.itemDragAndDropActivar.dragAndDropInit("ITEM");
     }
 
+    /**
+     * Detiene la activación del drag and drop del {@link Item}.
+     */
     public void dragAndDropCancel() {
         this.itemDragAndDropActivar.dragAndDropCancel();
     }
 
+    /**
+     * Captura el movimiento del mouse para mover el {@link Item}.
+     *
+     * @param mouseDraggedEvent
+     */
     public void dragAndDropMouseDragged(java.awt.event.MouseEvent mouseDraggedEvent) {
         this.itemDragAndDropActivar.setMouseDraggedOnItem(mouseDraggedEvent);
 
     }
 
+    /**
+     * Duración en milisegundos que debe pasar para activar el drag and drop del
+     * {@link Item}.
+     *
+     * @param milis duración hasta la activación del drag and drop del
+     * {@link Item}.
+     */
     void setDragAndDropAnimationTime(int milis) {
         this.itemDragAndDropActivar.dragAndDropSetAnimationTime(milis);
     }
 
-
     //##########################################################################
     /**
-     * Para animar ocultar o mostrar los subItems del Item desde ToggleList
+     * Para animar ocultar o mostrar los {@link Item.SubItem} del {@link Item}
+     * usando {@link ToggleList}
      *
-     * @param components Lista de subItems
-     * @param animated valor de animacion desde ToggleList
-     * @param selected determina si se oculta o se muestra los subItems del
-     * Item.
+     * @param components Lista de {@link Item.SubItem}
+     * @param animated valor de animacion en el {@link ToggleList}
+     * @param selected determina si se oculta o se muestra los
+     * {@link Item.SubItem} del {@link Item}.
      */
     void show(List<Component> components, float animated, boolean selected) {
         int width = (int) (100f * animated);
@@ -276,8 +355,7 @@ public abstract class Item extends JPanel implements DnD_AnimationTimeCurrentVal
         for (Component com : components) {
             int height = (int) (com.getPreferredSize().height * animated);
             getToggleListLayout().getMigLayout().setComponentConstraints(com, "h " + height + "!, w " + width + "%!, gap " + gap + "%! n 0px 0px");
-            if (com instanceof Item) {
-                Item item = (Item) com;
+            if (com instanceof Item item) {
                 if (item.getSubItems() != null) {
                     if ((!selected && item.getToggleList().isShowing()) || (selected && item.getToggleList().isShowing())) {
                         show(item.getSubItems(), animated, item.getToggleList().isShowing());
@@ -310,14 +388,16 @@ public abstract class Item extends JPanel implements DnD_AnimationTimeCurrentVal
     }
 
     /**
-     * Clase donde se manejan los Items y SubItems, Se asigna de manera
-     * automática, no tocar. Principalmente aquí para las animaciones de mostrar
-     * u ocultar los subItems del Item.
+     * Clase donde se manejan los {@link Item} y {@link Item.SubItem}. Se asigna
+     * de manera automática en el {@link ToggleListAnimationLayout}, no tocar.
+     * Principalmente aquí para las animaciones de mostrar u ocultar los
+     * subItems del Item monstrando los valores actuales que concurre la
+     * animacion correspondiente a ocultar o mostrar.
      *
-     * @param toggleListLayout
+     * @param toggleListLayout se le pasa el {@link ToggleListAnimationLayout}
      */
     void setToggleListLayout(ToggleListAnimationLayout toggleListLayout) {
-        this.toggleListLayout = toggleListLayout;
+        Item.toggleListLayout = toggleListLayout;
         getToggleList().addToggleListListener(new ToggleListAdapter() {
             @Override
             public void onAnimated(float animated) {
@@ -340,9 +420,9 @@ public abstract class Item extends JPanel implements DnD_AnimationTimeCurrentVal
 
     //##########################################################################
     /**
-     * Para extender a una clase JPanel para tomarlo como subItem en el layout
+     * Para extender a una clase JPanel para tomarlo como {@link Item.SubItem} en el layout
      * {@link ToggleListAnimationLayout}. Ejm: public class miPanel extends
-     * JPanel{} -> public class miPanel extends Item.SubItem{}
+     * JPanel{} replace with-> public class miPanel extends Item.SubItem{}
      */
     public abstract static class SubItem extends JPanel implements DnD_AnimationTimeCurrentValue {
 
